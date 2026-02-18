6dba442e75ad06122321bc633272bc9d9e1861f8
KayasSecret <kayasmishra29s@gmail.com>
2026-02-18 17:19:37 +0530

    #Update

---
+++ Fourth (Loop)/5.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i, num, rev = 0, rem;
+
+    cout << "Enter the number : ";
+    cin >> num;
+
+    while(num != 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num/10;
+    }
+
+    cout << rev;
+
+    return 0;
+}
