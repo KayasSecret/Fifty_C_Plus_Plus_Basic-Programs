5129e5fb5ad33e9ba90efcd7886fb1bb7fd0905a
KayasSecret <kayasmishra29s@gmail.com>
2026-02-19 10:22:57 +0530

    #New_File_Update

---
+++ Fifth (Loop)/3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i, j, n;
+
+    cout << "Enter the number of terms : ";
+    cin >> n;
+
+    for(i=0; i<n; i++) {
+        for(j=0; j<=i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
