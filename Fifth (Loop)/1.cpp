461c98281ea0144216f4fdb38e8e8a1bc04ed8f1
KayasSecret <kayasmishra29s@gmail.com>
2026-02-19 10:14:18 +0530

    #New_File

---
+++ Fifth (Loop)/1.cpp
@@ -0,0 +1,26 @@
+/*
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * * 
+   * * * * *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i, j, n;
+
+    cout << "Enter the number of terms : ";
+    cin >> n;
+
+    for(i=0; i<n; i++) {
+        for(j=0; j<n; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
