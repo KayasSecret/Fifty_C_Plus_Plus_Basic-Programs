4d77c2bbcad4077e418ddf30197a28312d842b26
KayasSecret <kayasmishra29s@gmail.com>
2026-02-22 18:19:19 +0530

    #Update

--- Sixth (Loop)/7.cpp
+++ Sixth (Loop)/7.cpp
@@ -1,30 +1,31 @@
 /*
-  *         *
-   *       *
-    *     *
-     *   *   
-       * 
-      *  *
-     *    *
-    *      *   
-   *        *   
+  *               *
+    *           *
+      *       *
+        *   *   
+          * 
+         *  *
+       *      *
+     *          *   
+   *              *   
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int i, j, n, m;
+    int i, j, n;
 
-    cout << "Enter the number of rows : ";
+    cout << "Enter the number of terms : ";
     cin >> n;
 
-    cout << "Enter the number of columns : ";
-    cin >> m;
-
     for(i=1; i<=n; i++) {
-        for(j=1; j<=m; j++) {
-            cout << "* ";
+        for(j=1; j<=n; j++) {
+            if(i==j || i+j == n+1) {
+                cout << "* ";
+            } else { 
+                cout << "  ";
+            }
         }
         cout << endl;
     }
