bcbeec453c4d9e00d34f3031bee9701b27f52bc7
KayasSecret <kayasmishra29s@gmail.com>
2026-02-25 17:17:27 +0530

    #Update

--- Function/Pass By Reference/1.cpp
+++ Function/Pass By Reference/1.cpp
@@ -10,13 +10,15 @@
 using namespace std;
 
 void swapNumbers(int &a, int &b) {
-    int *temp = &a;
-    &a = &b;
-    &b = temp;
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main() {
     int x = 10, y = 20;
+
+    cout << x << " " << y << endl;
 
     swapNumbers(x, y);
 
