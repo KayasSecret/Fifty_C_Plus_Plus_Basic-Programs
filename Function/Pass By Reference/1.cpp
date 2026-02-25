53ff2ba37364aca356ae8d64220d2a50b2f2c5ba
KayasSecret <kayasmishra29s@gmail.com>
2026-02-25 16:31:20 +0530

    #Update

---
+++ Function/Pass By Reference/1.cpp
@@ -0,0 +1,26 @@
+/*
+- Swap Without Return
+👉 Create a function swapNumbers()
+👉 Use pass by reference
+👉 Swap two numbers
+👉 Print values before and after swap in main
+*/
+
+#include<iostream>
+using namespace std;
+
+void swapNumbers(int &a, int &b) {
+    int *temp = &a;
+    &a = &b;
+    &b = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    swapNumbers(x, y);
+
+    cout << x << " " << y;
+
+    return 0;
+}
