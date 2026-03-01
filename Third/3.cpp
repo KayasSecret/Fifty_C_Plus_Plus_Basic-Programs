f8ee6eed750a5eeb5bf25f06bbdf54f875bf11a0
KayasSecret <kayasmishra29s@gmail.com>
2026-03-01 08:38:22 +0530

    #Update

--- Third/3.cpp
+++ Third/3.cpp
@@ -15,7 +15,17 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    
+    // sum of all elements 
+    for(i=0; i<n; i++) {
+        sum = sum + arr[i];
+    }
+
+    printf("This is the array : \n");
+    for(i=0; i<n; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    printf("\nThis is the total of elements : %d", sum);
 
     return 0;
 }
