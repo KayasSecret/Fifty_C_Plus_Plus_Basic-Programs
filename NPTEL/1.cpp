481ac5c244c22f3132ff7e3fa89630d8cb93356a
KayasSecret <kayasmishra29s@gmail.com>
2026-03-03 14:12:26 +0530

    #Update

---
+++ NPTEL/1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class B{
+protected:
+    int val;
+public:
+    B(int v);
+    virtual ~B();          // LINE-1
+    int getVal() { return val; }
+};
+
+class D : public B{
+public:
+    D(int v);
+    ~D();                  // LINE-2
+};
+
+B::B(int v) : val(v){
+    cout << val << " ";
+}
+
+B::~B(){
+    cout << val * 10 << " ";
+}
+
+D::D(int v) : B(v){
+    cout << val * 20 << " ";
+}
+
+D::~D(){
+    cout << getVal() << " ";
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    D *ptr = new D(n);
+    B *bptr = ptr;
+    delete bptr;
+
+    return 0;
+}
