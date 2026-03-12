12b479b6dec182d1b00e4e84d08115a1a463981a
KayasSecret <kayasmishra29s@gmail.com>
2026-03-12 18:14:18 +0530

    #Update

--- Second/char.cpp
+++ Second/char.cpp
@@ -7,7 +7,13 @@ int main() {
     cout << "Entet any char - ";
     cin >> ch;
 
-   
+    if((ch >= 'a') && (ch <= 'z')) {
+        cout << "Lowercase\n";
+    } else if((ch >= 'A') && (ch <= 'Z')) {
+        cout << "Uppercase\n";
+    } else {
+        cout << "Enter a valid Character";
+    }
 
     return 0;
 }
